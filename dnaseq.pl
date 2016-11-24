eaccda0d6310b770d25d169d7055ac1605641c82
linux <linux@localhost.localdomain>
2016-11-24 10:18:51 +0800

    first commit

--- dnaseq.pl
+++ dnaseq.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/perl -w
-$DNA = 'ACGGGAGGACGGGAAAATTACTACGGCATTAGC';
-print $DNA;
+$DNA1 = 'ACGGGAGGACGGGAAAATTACTACGGCATTAGC';
+$DNA2 = 'ATAGTCCGTGAGATGTAGTA';
+print "Here are the original two DNA fragments:\n\n";
+print $DNA1,"\n";
+print $DNA2,"\n\n";
+$DNA3 = "$DNA1$DNA2";
+print "Here is the concatenation of the first two fragments:\n\n";
+print "$DNA3\n\n";
 exit;
