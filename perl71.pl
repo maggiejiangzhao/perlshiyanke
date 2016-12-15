dd40bc034d5911708f218d46db0c9b637d98bff8
linux <linux@localhost.localdomain>
2016-12-15 08:23:25 +0800

    perl

---
+++ perl71.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+my @nucleotides = ('A', 'C', 'G', 'T');
+srand(time|$$);
+for (my $i=0 ; $i<20 ; ++$i ) {
+print randomnucleotide(@nucleotides), " ";
+}
+print "\n";
+exit;
+sub randomnucleotide {
+my(@nucs) = @_;
+return $nucs[rand @nucs];
+}
+
